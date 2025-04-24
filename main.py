14b1aea3980ba6ea02e7b4adae659c5add6e97b2
David Márquez <dmarpoz833@g.educaand.es>
2025-04-24 10:55:02 +0100

    Actualizando archivo main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from leer_partidos import LeerPartidos  # Importamos la función para leer los partidos
 from imp_clasificacion import impClasificacion  # Importamos la función para imprimir la clasificación
 
-# Punto de entrada del programa
-if __name__ == '__main__':
-    liga = LeerPartidos()  # Leemos los partidos desde el archivo CSV
-    impClasificacion(liga)  # Mostramos la clasificación final
+liga = LeerPartidos()  # Leemos los partidos desde el archivo CSV
+impClasificacion(liga)  # Mostramos la clasificación final
